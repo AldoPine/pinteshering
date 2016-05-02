397be063f8e06504effb96e0a928ee9c71fa3f8f
aldopine <aldopineda_m@yahoo.com.mx>
2016-05-02 16:40:31 -0500

    Implemented Devise for Users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  #Require for Heroku
+  config.action_mailer.default_url_options = { host: 'https://morning-refuge-75651.herokuapp.com/' }
 end
