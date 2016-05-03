06d7ef3fa32d4671155c4c2c54f5f680d6c09bf4
aldopine <aldopineda_m@yahoo.com.mx>
2016-05-03 15:56:52 -0500

    Added Pins

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160502211757) do
+ActiveRecord::Schema.define(version: 20160503204717) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
