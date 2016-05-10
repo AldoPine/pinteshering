8ba20ad3e14e1fd8eba822e3e7af12cd0a6ebd49
aldopine <aldopineda_m@yahoo.com.mx>
2016-05-10 15:51:16 -0500

    Add names to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :pins
+  validates :name, presence: true
 end
